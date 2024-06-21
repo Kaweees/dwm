92fa26d3f4aafe70598d0a632cdf7d183cbf3285
Miguel Villa Floran <miguel.villafloran@gmail.com>
2024-06-20 18:11:24 -0700

    Added side button

--- dwm.c
+++ dwm.c
@@ -88,6 +88,7 @@ enum {
   ClkLtSymbol,
   ClkStatusText,
   ClkWinTitle,
+  ClkButton,
   ClkClientWin,
   ClkRootWin,
   ClkLast
@@ -428,17 +429,22 @@ void buttonpress(XEvent *e) {
   }
   if (ev->window == selmon->barwin) {
     i = x = 0;
-    do x += TEXTW(tags[i]);
-    while (ev->x >= x && ++i < LENGTH(tags));
-    if (i < LENGTH(tags)) {
-      click = ClkTagBar;
-      arg.ui = 1 << i;
-    } else if (ev->x < x + TEXTW(selmon->ltsymbol))
-      click = ClkLtSymbol;
-    else if (ev->x > selmon->ww - (int)TEXTW(stext))
-      click = ClkStatusText;
-    else
-      click = ClkWinTitle;
+    x += TEXTW(buttonbar);
+    if (ev->x < x) {
+      click = ClkButton;
+    } else {
+      do x += TEXTW(tags[i]);
+      while (ev->x >= x && ++i < LENGTH(tags));
+      if (i < LENGTH(tags)) {
+        click = ClkTagBar;
+        arg.ui = 1 << i;
+      } else if (ev->x < x)
+        click = ClkLtSymbol;
+      else if (ev->x > selmon->ww - TEXTW(stext))
+        click = ClkStatusText;
+      else
+        click = ClkWinTitle;
+    }
   } else if ((c = wintoclient(ev->window))) {
     focus(c);
     restack(selmon);
@@ -686,6 +692,9 @@ void drawbar(Monitor *m) {
     if (c->isurgent) urg |= c->tags;
   }
   x = 0;
+  w = TEXTW(buttonbar);
+  drw_setscheme(drw, scheme[SchemeNorm]);
+  x = drw_text(drw, x, 0, w, bh, lrpad / 2, buttonbar, 0);
   for (i = 0; i < LENGTH(tags); i++) {
     w = TEXTW(tags[i]);
     drw_setscheme(
